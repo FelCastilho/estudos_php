67f3b1c43d54b6d724a8802dc201cbc37a6b0dc9
felipeCast <castfelipe10@gmail.com>
2024-08-26 08:13:07 -0300

    aprofundando estudo de variaveis e constantes

---
+++ CursoEmVideo/Modulo01/001.php
@@ -0,0 +1,11 @@
+<?php 
+    $nome = "felipe";
+    $sobrenome = "coutinho";
+
+    //constantes devem ser escritas em maiúsculo;
+    const PAIS = "Brasil";
+    
+
+    //interpolação com constantes deve ser feita com: .
+    echo "Muito prazer $nome $sobrenome! Você mora no " .PAIS; 
+?>
